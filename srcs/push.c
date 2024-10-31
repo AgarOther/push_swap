c3a60f72eee7f063345df6810daa40e1ffd98858
AgarOther <agarother@gmail.com>
2024-10-31 10:16:18 +0100

    Fixed push

--- srcs/push.c
+++ srcs/push.c
@@ -6,7 +6,7 @@
 /*   By: scraeyme <scraeyme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 17:03:35 by scraeyme          #+#    #+#             */
-/*   Updated: 2024/10/31 09:43:14 by scraeyme         ###   ########.fr       */
+/*   Updated: 2024/10/31 10:14:45 by scraeyme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 void	pa(t_list **stack_a, t_list **stack_b, int is_pb)
 {
-	while ((*stack_a)->next->next)
-		*stack_a = (*stack_a)->next;
+	t_list	*tmp;
+
+	tmp = *stack_a;
+	while (tmp->next->next)
+		tmp = tmp->next;
 	ft_lstlast(*stack_b)->next = (*stack_a)->next;
 	(*stack_a)->next = NULL;
 	if (!is_pb)
